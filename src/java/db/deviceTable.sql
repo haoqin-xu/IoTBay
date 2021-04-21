47a807251a418726cbb9e53b6e15830c7dc9e5a0
briana00 <briana.r.margetts@student.uts.edu.au>
2021-04-21 14:38:07 +1000

    added create device table

--- src/java/db/deviceTable.sql
+++ src/java/db/deviceTable.sql
@@ -8,3 +8,12 @@
  * Created: 21/04/2021
  */
 
+CREATE TABLE DEVICE (
+    DEVICE_ID INTEGER(10) NOT NULL,
+    "NAME" VARCHAR(20) NOT NULL,
+    MANUFACTURER VARCHAR(20),
+    ORIGIN VARCHAR(20),
+    INSTOCK BIT(1), /* when inserting values 1 = instock and 0 = not instock*/
+    STOCK_NUM INTEGER(10) NOT NULL,
+    PRIMARY KEY(DEVICE_ID) 
+);
