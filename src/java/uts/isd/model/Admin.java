b16a293ce7481e400d187a0aec2a3f68ad4cd82e
haoqin-xu <68982806+haoqin-xu@users.noreply.github.com>
2021-05-04 18:49:05 +1000

    Add constructor/getter/setter to admin model

--- src/java/uts/isd/model/Admin.java
+++ src/java/uts/isd/model/Admin.java
@@ -12,11 +12,99 @@ import java.io.Serializable;
  * @author TED
  */
 public class Admin implements Serializable {
+    private int ID;
     private String email;
     private String name;
     private String password;
     private String dob;
     private String phone;
     private String address;
-    
+    private String role;
+
+    // constructor without ID (for object creation)
+    public Admin(String email, String name, String password, String dob, String phone, String address, String role) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.dob = dob;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
+
+    // constructor with ID (for session storage)
+    public Admin(int ID, String email, String name, String password, String dob, String phone, String address, String role) {
+        this.ID = ID;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.dob = dob;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
