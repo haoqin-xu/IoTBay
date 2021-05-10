cd6eee332761851b69ad26894ecaa4a45d7c3bbf
briana00 <briana.r.margetts@student.uts.edu.au>
2021-05-10 17:26:07 +1000

    Create DBdrop.sql
    script to drop all tables

---
+++ src/java/db/DBdrop.sql
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Briana Margetts
+ * Created: 10/05/2021
+ */
+DROP TABLE "USER";
+
+DROP TABLE "ADMIN";
+
+DROP TABLE CUSTOMER;
+
+DROP TABLE "ORDER";
+
+DROP TABLE DEVICE;
+
+DROP TABLE "INVOICE";
+
+DROP TABLE ORDERLINEITEM;
+
+DROP TABLE "PAYMENT";
+
+DROP TABLE SHIPMENT;
+
+DROP TABLE STAFF;
+
+
+
+
