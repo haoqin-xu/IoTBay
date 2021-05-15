be4e903061cab1e9f9b5c50d98be4a198dc20f23
leduc1234 <Duc.T.Le@student.uts.edu.au>
2021-05-15 13:54:51 +1000

    added comments to order controller

--- src/java/uts/isd/controller/OrderController.java
+++ src/java/uts/isd/controller/OrderController.java
@@ -37,50 +37,43 @@ public class OrderController extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        //1- retrieve the current session
+        //retrieve the current session
         HttpSession session = request.getSession();
+        // initialise DAO connection
         OrderLineManager orderlinemanager = (OrderLineManager)session.getAttribute("orderlinemanager");
         OrderManager manager = (OrderManager) session.getAttribute("ordermanager");
         DeviceManager devicemanager = (DeviceManager) session.getAttribute("devicemanager");
         
-         
+        //create order validator to validate inputs
         OrderValidator validator = new OrderValidator();
-        validator.clear(session);
         
-        
+        // retrieve customer object from session
         Customer customerObj = (Customer) session.getAttribute("user");
-        
+        // retrieve customerid from customer object
         int customerid = customerObj.getID();
-       
+        // retrieve payment id from session
         int paymentid = Integer.parseInt(session.getAttribute("randompaymentid").toString());
-       
-        
-        
-        
+        // initialise deviceid and count variable
         int deviceid;
         int count;
+        // retrieve invoice from session
         int invoiceid = Integer.parseInt(session.getAttribute("invoiceid").toString());
-        
+        // retrieve date from session
         String date = session.getAttribute("date").toString();
-        
+        // clear all the validation inputs for new inputs
         validator.clear(session);
-        //5- retrieve the manager instance from session
-       
-        PrintWriter ps = response.getWriter();
-        ps.print(customerid);
-     
+        
         try {
+            // check the inputs and catch if incorrect format in numberformatexception catch
             deviceid = Integer.parseInt(request.getParameter("deviceid"));
             count = Integer.parseInt(request.getParameter("count"));
+            // if input passes then set attribute to order is successful
             session.setAttribute("successful", "Order successful");
+            // update devicecount,createorder,addorderline updates the order, orderline and device tables
             devicemanager.updateDeviceCount(deviceid,count);
             manager.createOrder(customerid, paymentid, deviceid, invoiceid, date);
             orderlinemanager.addOrderline(deviceid,count);
-            
-            
-           
-         
-            
+
         } catch(NumberFormatException exd){
             //catch numberformat exception when user inputs string or blank
             session.setAttribute("notint", "Error: format incorrect");
@@ -89,6 +82,7 @@ public class OrderController extends HttpServlet {
         catch (SQLException ex) {
             Logger.getLogger(OrderController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        // if try statement works, forwards back to create order to choose to create more orders or view orders
         request.getRequestDispatcher("CreateOrder.jsp").forward(request, response);
         
         
